5240a9794a9a1c54923b9080fcc673a835a11be8
Blake Gardiner <33143061+blakegards@users.noreply.github.com>
2018-04-02 14:18:46 +0700

    changed arcgis to geopandas plugin.

--- LightningDetection.py
+++ LightningDetection.py
@@ -1,27 +1,53 @@
 #--------------------------------------------------------------------------------------------
 # LightningDetection.py
-# VERSION: 0.0.1 - Feb 14, 2018
+# VERSION: 0.0.2 - March 22, 2018
 # AUTHOR: Blake Gardiner for People in Need  - blake.gardiner@peopleinneed.cz
 #
 # PARAMETERS: 
 # DEPENDENCIES: untangle - pip install untangle
-# ABOUT:
-# LIMITATIONS: Relies on ArcPy and therefore an ESRI ArcGIS installation.
+#                geopandas - reccomend to install via conda 'pip install geopandas'.
+#                http://geopandas.org/install.html#dependencies
+#                major pain setting up these environments....
+# ABOUT:        This script assesses the JSON/XML response from Earth Networks
+#                weather output. By overlaying the 'Warning' polygon (if any) with
+#                Cambodian commune boundaries, we can identify at-risk communes.
+#               Uses GeoPandas open source library to avoid ESRI stack reliance.
+# FUTURE:       
+# LIMITATIONS: - Ensure both datasets are the same cooridnate reference system, as 
+#                geopandas does not support on the fly projection.
+#              - The JSON response contains XML formatted data which is unusual.
 #--------------------------------------------------------------------------------------------
 # KNOWN ISSUES
-# No subscription key currently, just read from supplied json file.
+# No subscription key currently, just read from supplied json file. Module 'getResponse' is
+# sourced from Earth Networks documentation but has not been tested.
+#
+# Some of the test JSON files are not in the Cambodia area, therefore no overlap will exist.
+#
 #--------------------------------------------------------------------------------------------
-import http.client, urllib.request, urllib.parse, urllib.error, base64, json, urllib.request
-import shapefile
+import http.client, urllib.request, urllib.parse, urllib.error, base64, json, urllib.request, sys, os
+import geopandas as gp, shapefile
 from idlelib.rpc import response_queue
 import untangle
 
-
 def main():
-    # No api key :(
-    #getResponse("9.85521608608867,-80.70281982421876")
+    #USER DEFINED VARIABLES ###########################################################
     sourceJSONFile = "pplnneed_dta_20170908_090828.json"
-    processResponse(sourceJSONFile)
+    storm_warning_area_polygon = r"C:\Users\Blake\Documents\Blake\LiClipse Workspace\ERLightning\TESTPOLY2Cambodia.shp"
+    cambodia_commune_polygon = r"C:\Users\Blake\Documents\Blake\LiClipse Workspace\ERLightning\khm_adm3_wgs84.shp"
+    
+    global WarningPoly #storm cell warning area polggon
+    WarningPoly = "WARNING_AREA"
+    global StormPoly #storm cell area polygon
+    StormPoly = "STORMCELL"
+    global CRS #reference system for new polygons
+    CRS = "4326"
+    #END USER DEFINED VARIABLES ########################################################
+    
+    #Process the JSON, create storm cell and warning area polygons.
+    if processResponse(sourceJSONFile):
+        #See if the warning polygon overlaps any Cambodian Commmunes.
+        #Print a list.
+        geopanda_analysis(cambodia_commune_polygon, storm_warning_area_polygon)
         
 def getResponse(LatLong):
     headers = {
@@ -46,100 +72,100 @@ def getResponse(LatLong):
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
         
 def processResponse(jsonResponse):
-    ENresponse = json.load(open(jsonResponse))
-    XMLresponse = (ENresponse['RawMessage'])
-    #print(XMLresponse)
-
-    obj = untangle.parse(XMLresponse)
-    #print(obj.alert.identifier.cdata)
-    print("Lightning Severity: " + str(obj.alert.info.parameter[2].value)) #cell speed
-    print("Cell Speed: " + str(obj.alert.info.parameter[4].value)) #cell speed
-    
-    coordinates = str(obj.alert.info.area.polygon.cdata).split(", ") #split string into a list
-    LatLongParts = []
-    print("## Area Polygon ##")
-    for pair in coordinates:
-        latlong = pair.split(" ")
-        #print("Lat: " + latlong[0] + " Long: " + latlong[1])
-        LatLongParts.append([float(latlong[1]),float(latlong[0])])
-    
-    print(LatLongParts)
-    w = shapefile.Writer(shapefile.POLYGON)
-    w.poly(parts=[LatLongParts])
-    w.field('FIRST_FLD','C','40')
-    w.field('SECOND_FLD','C','40')
-    w.record('Poly','PolyTest')
-    w.save('WARNING_AREA')
-
-    # create the .prj file
-    prj = open("WARNING_AREA.prj", "w")
-    # call the function and supply the epsg code
-    epsg = getWKT_PRJ("4326")
-    prj.write(epsg)
-    prj.close()   
-    
-    
-    
-    # now write a polygon for the stormcell polygon
-    #print(obj.alert.info.parameter[2].value.cdata) #stormcell polygon
-    stormcellPoly = obj.alert.info.parameter[2].value.cdata
-    stormcellPoly = stormcellPoly[:-2] #cleanup result
-    stormcellPoly = stormcellPoly[10:] #cleanup result
-    stormcellPoly = stormcellPoly.split(", ") #split string into a list
-    print("## Stormcell Polygon ##")
-    LatLongParts = []
-    for pair in stormcellPoly:
-        latlong = pair.split(" ")
-        #print("Lat: " + latlong[0] + " Long: " + latlong[1])
-        LatLongParts.append([float(latlong[1]),float(latlong[0])])
+    try:
+        ENresponse = json.load(open(jsonResponse))
+        if not (ENresponse == "{}"): #ensure the response is not blank
+            XMLresponse = (ENresponse['RawMessage'])
+            obj = untangle.parse(XMLresponse)
+            #print(obj.alert.identifier.cdata)
+            #print("Lightning Severity: " + str(obj.alert.info.parameter[2].value)) #cell speed
+            #print("Cell Speed: " + str(obj.alert.info.parameter[4].value)) #cell speed
+            
+            coordinates = str(obj.alert.info.area.polygon.cdata).split(", ") #split string into a list
+            LatLongParts = []
+            print("## Area Polygon ##")
+            for pair in coordinates:
+                latlong = pair.split(" ")
+                LatLongParts.append([float(latlong[1]),float(latlong[0])])
+            
+            print(LatLongParts)
+            w = shapefile.Writer(shapefile.POLYGON)
+            w.poly(parts=[LatLongParts])
+            w.field('FIRST_FLD','C','40')
+            w.field('SECOND_FLD','C','40')
+            w.record('Poly','PolyTest')
+            w.save(WarningPoly)
         
-    print(LatLongParts)
-    w = shapefile.Writer(shapefile.POLYGON)
-    w.poly(parts=[LatLongParts])
-    w.field('FIRST_FLD','C','40')
-    w.field('SECOND_FLD','C','40')
-    w.record('Poly','PolyTest')
-    w.save('STORMCELL')
-
-    # create the .prj file
-    prj = open("STORMCELL.prj", "w")
-    # call the function and supply the epsg code
-    epsg = getWKT_PRJ("4326")
-    prj.write(epsg)
-    prj.close()        
+            # create the .prj file
+            prj = open(WarningPoly + ".prj", "w")
+            # call the function and supply the epsg code
+            epsg = getWKT_PRJ(CRS)
+            prj.write(epsg)
+            prj.close()   
+            
+            # now write a polygon for the stormcell polygon
+            stormcellPoly = obj.alert.info.parameter[2].value.cdata
+            stormcellPoly = stormcellPoly[:-2] #cleanup result
+            stormcellPoly = stormcellPoly[10:] #cleanup result
+            stormcellPoly = stormcellPoly.split(", ") #split string into a list
+            print("## Stormcell Polygon ##")
+            LatLongParts = []
+            for pair in stormcellPoly:
+                latlong = pair.split(" ")
+                LatLongParts.append([float(latlong[1]),float(latlong[0])])
+                
+            print(LatLongParts)
+            w = shapefile.Writer(shapefile.POLYGON)
+            w.poly(parts=[LatLongParts])
+            w.field('FIRST_FLD','C','40')
+            w.field('SECOND_FLD','C','40')
+            w.record('Poly','PolyTest')
+            w.save(StormPoly)
         
+            # create the .prj file
+            prj = open(StormPoly + ".prj", "w")
+            # call the function and supply the epsg code
+            epsg = getWKT_PRJ(CRS)
+            prj.write(epsg)
+            prj.close()  
+            return True 
+    except Exception as e:
+       print ("There was an error while processing. " + String(e))
+       return False
+   
 def getWKT_PRJ (epsg_code):
-     # function to generate .prj file information using spatialreference.org
-     # access projection information
-     with urllib.request.urlopen("http://spatialreference.org/ref/epsg/{0}/prettywkt/".format(epsg_code)) as myurl:
-         wkt = myurl.read()    
-     remove_spaces = str(wkt).replace(" ","")
-     prjString = str(remove_spaces).replace("\\n", "")
-     prjString = prjString[2:-1]
-     return prjString
-
-def arcgis_selection():
-    # Import arcpy module
-    import arcpy
-    arcpy.env.workspace = "C:/Users/Blake/Documents/Blake/LiClipse Workspace/ERLightning"
-    
-    # Local variables:
-    communePoly = "khm_admbnda_adm3_gov.shp"
-    warningAreaPoly = "TESTPOLY2Cambodia.shp"
-        
-    # Make feature layer as input
-    arcpy.MakeFeatureLayer_management(communePoly, 'communeLayer')
-    # Process: Select Layer By Location
-    arcpy.SelectLayerByLocation_management('communeLayer', "INTERSECT", warningAreaPoly, "", "NEW_SELECTION", "NOT_INVERT")
-    
-    rows = arcpy.SearchCursor('communeLayer')
-    counter = 0
-    for row in rows:
-        counter += 1
-        print("Commune ["  + str(row.getValue('HRName') + "] is in the path of ER Lightning Warning Polygon!"))
-    print("A total of " + str(counter) + " communes are at risk from the approaching storm.")
-    
-
-        
+     # Generate .prj file information using spatialreference.org
+     # otherwise shapefile will have no spatial reference. 
+     try:
+         with urllib.request.urlopen("http://spatialreference.org/ref/epsg/{0}/prettywkt/".format(epsg_code)) as myurl:
+             wkt = myurl.read()    
+         remove_spaces = str(wkt).replace(" ","")
+         prjString = str(remove_spaces).replace("\\n", "")
+         prjString = prjString[2:-1]
+         return prjString
+     except Exception as e:
+       print ("There was an error in getWKT_PRJ function. " + String(e))
+      
+def geopanda_analysis(commune_polygon, warning_polygon):
+   # Select communes where the dataset intersects the warning area polygon.
+   # Print the communes at risk.
+   # Both datasets must be in the safe coordinate reference system for this to work.
+    try:
+        poly1 = gp.read_file(commune_polygon)
+        poly2 = gp.read_file(warning_polygon)
+        data = []
+        dangerFlag = False
+        for index, orig in poly1.iterrows():
+            for index2, ref in poly2.iterrows():    
+                if ref['geometry'].intersects(orig['geometry']): 
+                    dangerFlag = True
+                    owdspd=orig['COM_CODE']
+                    data.append({'geometry':ref['geometry'].intersection(orig['geometry']),'wdspd':owdspd})
+                    print("Commune " + str(owdspd) + " is within the storm warning area.")
+        if not dangerFlag:
+                print("There were no communes at risk from the storm.")
+    except Exception as e:
+       print ("There was an error in geopanda_analysis function. " + String(e))
+      
 if __name__ == "__main__":
     main()
